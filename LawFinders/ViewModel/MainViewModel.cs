8f8bda1b51f3f46ed2fc7d690ed8a35f6ebefb8f
joebrooks <lcw3176@naver.com>
2022-02-07 16:54:30 +0900

    내용 수정

--- LawFinders/ViewModel/MainViewModel.cs
+++ LawFinders/ViewModel/MainViewModel.cs
@@ -1,9 +1,6 @@
 ﻿using LawFinders.Command;
+using LawFinders.Service;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Windows.Input;
 
@@ -11,31 +8,62 @@ namespace LawFinders.ViewModel
 {
     public class MainViewModel
     {
-        WebBrowser browser;
-        public ICommand SearchCommand { get; set; }
-        public ICommand SelectCommand { get; set; }
+        private WebBrowser browser;
+        private bool isLoaded = false;
+        private readonly ExtractService extractService = new ExtractService();
+
+        public ICommand ExtractCommand { get; set; }
 
         public MainViewModel(WebBrowser browser)
         {
             this.browser = browser;
+
+            ExtractCommand = new RelayCommand(ExtractExecuteMethod);
             InitBrowser();
         }
+
+
+        /// <summary>
+        /// 데이터 외부 문서로 추출 커맨드
+        /// </summary>
+        /// <param name="obj"></param>
+        private void ExtractExecuteMethod(object obj)
+        {
+
+        }
+
+
         private void InitBrowser()
         {
             browser.Navigate("https://www.law.go.kr/");
+            browser.ScriptErrorsSuppressed = true;
             browser.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(browser_DocumentCompleted);
-            
         }
 
         private void browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
         {
-            browser.Document.Click += Document_Click;
+            if (!isLoaded)
+            {
+                browser.Document.Click += Document_Click;
+            }
+
+            isLoaded = true;
+            
         }
 
         private void Document_Click(object sender, HtmlElementEventArgs e)
         {
             HtmlElement elem = browser.Document.GetElementFromPoint(e.ClientMousePosition);
-            Console.WriteLine(elem.InnerText);
+
+            if(elem != null && elem.InnerText != null)
+            {
+                foreach(var i in extractService.GetRelatedLaws(elem.InnerText))
+                {
+                    Console.WriteLine(i.Jo + i.Hang + i.Ho);
+
+                }
+                
+            }
         }
 
     }
