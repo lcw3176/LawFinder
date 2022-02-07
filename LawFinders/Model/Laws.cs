8d0f2450528284c325f5a3697a252f8191e7f9d3
joebrooks <lcw3176@naver.com>
2022-02-07 16:54:11 +0900

    Laws 추가

---
+++ LawFinders/Model/Laws.cs
@@ -0,0 +1,9 @@
+﻿namespace LawFinders.Model
+{
+    internal class Laws
+    {
+        public string Jo { get; set; }
+        public string Hang { get; set; }
+        public string Ho { get; set; }
+    }
+}
