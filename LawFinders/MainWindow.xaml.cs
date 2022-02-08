558ced25adcf150c7ab5d64b95674d258f48d574
joebrooks <lcw3176@naver.com>
2022-02-08 13:45:43 +0900

    윈도우 중앙 정렬

--- LawFinders/MainWindow.xaml.cs
+++ LawFinders/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using LawFinders.ViewModel;
+using System;
+using System.Text.RegularExpressions;
 using System.Windows;
 
 namespace LawFinders
@@ -11,6 +13,9 @@ namespace LawFinders
         public MainWindow()
         {
             InitializeComponent();
+            this.Height = SystemParameters.PrimaryScreenHeight * 0.8;
+            this.Width = SystemParameters.PrimaryScreenWidth * 0.8;
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
             this.DataContext = new MainViewModel(browser);
         }
     }
