6b6ee169e04ddaa99dbb4a360110438e6eb4cca4
joebrooks <lcw3176@naver.com>
2022-02-09 00:09:14 +0900

    변수 변경

--- LawFinders/Model/Laws.cs
+++ LawFinders/Model/Laws.cs
@@ -1,10 +1,24 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 namespace LawFinders.Model
 {
-    internal class Laws
+    public class Laws
     {
-        public List<int> number { get; set; } = new List<int>();
-        public List<string> type { get; set; } = new List<string>();
+
+        public Dictionary<string, string> Data { get; set; } = new Dictionary<string, string>();
+
+
+        public string GetFullData()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach(string key in Data.Keys)
+            {
+                stringBuilder.Append(Data[key]);
+            }
+
+            return stringBuilder.ToString();
+        }
     }
 }
