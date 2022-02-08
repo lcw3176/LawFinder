bc45292759072aea298e7486f45e3d0f09a19574
joebrooks <lcw3176@naver.com>
2022-02-08 13:44:50 +0900

    BaseViewModel 추가

---
+++ LawFinders/ViewModel/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace LawFinders.ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
